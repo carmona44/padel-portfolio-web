41cdfc797230162e3b894150f73871f00edaa6c4
carmona44 <dcacarmona44@gmail.com>
2021-11-30 16:48:56 +0100

    feat: add custom default layout

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <SideBar />
+    <Nuxt />
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,12 @@
 <template>
-  <SideBar />
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 
-export default Vue.extend({});
+export default Vue.extend({
+  created() {
+    this.$router.push('/stats')
+  }
+});
 </script>
