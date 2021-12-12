361f762f8404d1aec3d8ca4f3815ca12345bbc6d
carmona44 <dcacarmona44@gmail.com>
2021-12-12 17:19:21 +0100

    refactor: player select inputs data from constants

--- components/modals/AddPlayerModal.vue
+++ components/modals/AddPlayerModal.vue
@@ -33,6 +33,7 @@
 import Vue from 'vue';
 import localforage from 'localforage';
 import { BIconPersonPlus } from 'bootstrap-vue';
+import { POSITIONS, LEVELS, DOMINANT_HAND, SHOTS } from '@/utils/constants/player';
 
 export default Vue.extend({
     components: { BIconPersonPlus },
@@ -47,45 +48,13 @@ export default Vue.extend({
             },
             nameState: <any> null,
             positionState: <any> null,
-            positions: [
-              { value: null, text: 'Selecciona una posición *' },
-              { value: 'SIN PREFERENCIA', text: 'SIN PREFERENCIA' },
-              { value: 'REVÉS', text: 'REVÉS' },
-              { value: 'DERECHA', text: 'DERECHA' }
-            ],
+            positions: POSITIONS,
             levelState: <any> null,
-            levels: [
-                { value: null, text: 'Selecciona un nivel *' },
-                { value: 1, text: '1ª - PRIMERA' },
-                { value: 2, text: '2ª - SEGUNDA' },
-                { value: 3, text: '3ª - TERCERA' },
-                { value: 4, text: '4ª - CUARTA' },
-                { value: 5, text: '5ª - QUINTA' }
-            ],
+            levels: LEVELS,
             dominantHandState: <any> null,
-            dominantHand: [
-                { value: null, text: 'Selecciona mano dominante *' },
-                { value: 'AMBIDIESTRO', text: 'AMBIDIESTRO' },
-                { value: 'DIESTRO', text: 'DIESTRO' },
-                { value: 'ZURDO', text: 'ZURDO' },
-            ],
+            dominantHand: DOMINANT_HAND,
             bestShotState: <any> null,
-            shots: [
-                { value: null, text: 'Selecciona su mejor golpe *' },
-                { value: 'VOLEA DERECHA', text: 'VOLEA DERECHA' },
-                { value: 'VOLEA REVÉS', text: 'VOLEA REVÉS' },
-                { value: 'BANDEJA', text: 'BANDEJA' },
-                { value: 'REMATE PLANO', text: 'REMATE PLANO' },
-                { value: 'REMATE LIFTADO', text: 'REMATE LIFTADO' },
-                { value: 'GLOBO', text: 'GLOBO' },
-                { value: 'DEJADA', text: 'DEJADA' },
-                { value: 'SAQUE', text: 'SAQUE' },
-                { value: 'DERECHA', text: 'DERECHA' },
-                { value: 'REVÉS', text: 'REVÉS' },
-                { value: 'RULO A LA REJA', text: 'RULO A LA REJA' },
-                { value: 'VÍBORA', text: 'VÍBORA' },
-                { value: 'CHIQUITA', text: 'CHIQUITA' },
-            ]
+            shots: SHOTS
         }
     },
     methods: {
---
+++ utils/constants/player.ts
@@ -0,0 +1,39 @@
+export const POSITIONS = [
+    { value: null, text: 'Selecciona una posición *' },
+    { value: 'SIN PREFERENCIA', text: 'SIN PREFERENCIA' },
+    { value: 'REVÉS', text: 'REVÉS' },
+    { value: 'DERECHA', text: 'DERECHA' }
+];
+
+export const LEVELS = [
+    { value: null, text: 'Selecciona un nivel *' },
+    { value: 1, text: '1ª - PRIMERA' },
+    { value: 2, text: '2ª - SEGUNDA' },
+    { value: 3, text: '3ª - TERCERA' },
+    { value: 4, text: '4ª - CUARTA' },
+    { value: 5, text: '5ª - QUINTA' }
+];
+
+export const DOMINANT_HAND = [
+    { value: null, text: 'Selecciona mano dominante *' },
+    { value: 'AMBIDIESTRO', text: 'AMBIDIESTRO' },
+    { value: 'DIESTRO', text: 'DIESTRO' },
+    { value: 'ZURDO', text: 'ZURDO' },
+];
+
+export const SHOTS = [
+    { value: null, text: 'Selecciona su mejor golpe *' },
+    { value: 'VOLEA DERECHA', text: 'VOLEA DERECHA' },
+    { value: 'VOLEA REVÉS', text: 'VOLEA REVÉS' },
+    { value: 'BANDEJA', text: 'BANDEJA' },
+    { value: 'REMATE PLANO', text: 'REMATE PLANO' },
+    { value: 'REMATE LIFTADO', text: 'REMATE LIFTADO' },
+    { value: 'GLOBO', text: 'GLOBO' },
+    { value: 'DEJADA', text: 'DEJADA' },
+    { value: 'SAQUE', text: 'SAQUE' },
+    { value: 'DERECHA', text: 'DERECHA' },
+    { value: 'REVÉS', text: 'REVÉS' },
+    { value: 'RULO A LA REJA', text: 'RULO A LA REJA' },
+    { value: 'VÍBORA', text: 'VÍBORA' },
+    { value: 'CHIQUITA', text: 'CHIQUITA' },
+];
